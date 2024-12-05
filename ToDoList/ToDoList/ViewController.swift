c580e43c30f72154fcf1dc2ebdf9989494b02fe0
Chumakov Aleksey <Alexeyse10@gmail.com>
2024-12-05 12:32:45 +0700

    Create base project, removed storyboard references

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Алексей Чумаков on 05.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
